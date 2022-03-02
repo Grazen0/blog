31a764d5aff4507923ef1081bed110110f34e204
ElCholoGamer <josedanielgrayson@gmail.com>
2022-03-02 18:51:20 -0500

    refactor(hooks): toggle dark mode class on html instead of body

--- src/lib/hooks/use-dark-mode-initial.ts
+++ src/lib/hooks/use-dark-mode-initial.ts
@@ -7,7 +7,7 @@ function useDarkModeinitial(): [boolean, SetState<boolean>] {
 	const [darkMode, setDarkMode] = useLocalStorage('dark_mode', true);
 
 	useEffect(() => {
-		const { classList } = document.body;
+		const { classList } = document.documentElement;
 		if (darkMode) classList.add(DARK_CLASS);
 		else classList.remove(DARK_CLASS);
 	}, [darkMode]);
