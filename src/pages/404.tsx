63cc370f3079da9b1512b9c4fcf4c8af9c05113d
ElCholoGamer <josedanielgrayson@gmail.com>
2022-03-02 23:18:42 -0500

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,30 @@
+import AnimatedLink from 'components/AnimatedLink';
+import Layout from 'components/layout/Layout';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next/types';
+
+const NotFound: NextPage = () => {
+	const router = useRouter();
+
+	const goBack = () => router.back();
+
+	return (
+		<Layout title="Page Not Found" description="Maybe you misspelled something?">
+			<main className="p-6">
+				<h1 className="text-5xl text-center my-8 font-bold">Page Not found :(</h1>
+				<p className="text-center text-xl">Maybe you misspelled something?</p>
+
+				<div className="text-center mx-auto my-12">
+					<AnimatedLink className="mx-4 text-lg font-semibold" href="/">
+						Home
+					</AnimatedLink>
+					<AnimatedLink className="mx-4 text-lg font-semibold" href="" onClick={goBack}>
+						Go back
+					</AnimatedLink>
+				</div>
+			</main>
+		</Layout>
+	);
+};
+
+export default NotFound;
