0a6fce4dfba2089725eb7291cb4db84df095f9ac
ElCholoGamer <josedanielgrayson@gmail.com>
2022-03-02 23:47:24 -0500

    refactor(home): reduce max latest posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const getStaticProps: GetStaticProps<Props> = async () => {
 	return {
 		props: {
-			latestPosts: getLatestPosts(10),
+			latestPosts: getLatestPosts(5),
 		},
 	};
 };
@@ -20,8 +20,8 @@ const Home: NextPage<Props> = ({ latestPosts }) => {
 	return (
 		<Layout title="Home">
 			<main className="p-6">
-				<h1 className="text-5xl text-center font-bold my-8">👋 Welcome to my blog</h1>
-				<p className="text-center text-lg">Glad to see you around!</p>
+				<h1 className="text-5xl text-center font-bold my-8">👋 Welcome to my blog!</h1>
+				<p className="text-center text-lg">Glad to see you around</p>
 
 				<div className="max-w-2xl mx-auto my-12">
 					<h2 className="text-2xl font-semibold">Latest posts:</h2>
