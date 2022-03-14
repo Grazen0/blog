d3661f76ed21f73524adf63d7908dabc98c9b528
ElCholoGamer <josedanielgrayson@gmail.com>
2022-03-14 18:08:22 -0500

    refactor(components): increase Card horizontal padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,7 +25,6 @@ const Card: React.FC<Props> = ({
 		{...props}
 		className={classNames(
 			className,
-			// 'my-8 overflow-hidden rounded-lg flex flex-col xs:flex-row justify-between bg-neutral-200 dark:bg-slate-700 shadow-intense dark:shadow-none'
 			'my-8 overflow-hidden rounded-lg grid grid-rows-[minmax(0,1.25fr)_minmax(0,1fr)] xs:grid-rows-none xs:grid-cols-[minmax(0,1fr)_minmax(0,2fr)] bg-neutral-200 dark:bg-slate-700 shadow-intense dark:shadow-none'
 		)}
 	>
@@ -34,7 +33,7 @@ const Card: React.FC<Props> = ({
 				<Image src={image} alt={imageAlt} layout="fill" className="object-cover text-center" />
 			</span>
 		)}
-		<span className="px-3 py-4 flex-grow">
+		<span className="px-6 py-4 flex-grow">
 			{head && <p className="text-md mb-2">{head}</p>}
 			<h3 className="text-2xl xs:text-3xl mb-3 font-semibold">{title}</h3>
 			<p className="xs:text-xl">{description}</p>
