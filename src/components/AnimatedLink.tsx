ce32b85e1f935a1fab4463bd28d32fa86e0c06ce
ElCholoGamer <josedanielgrayson@gmail.com>
2022-03-14 20:45:07 -0500

    feat(components): add external prop to link

--- src/components/AnimatedLink.tsx
+++ src/components/AnimatedLink.tsx
@@ -4,12 +4,23 @@ import classNames from 'classnames';
 
 export interface Props extends HTMLProps<HTMLAnchorElement> {
 	href: string;
+	external?: boolean;
 }
 
-const AnimatedLink: React.FC<Props> = ({ href, key, className, children, ...props }) => (
+const AnimatedLink: React.FC<Props> = ({
+	href,
+	key,
+	external = false,
+	className,
+	children,
+	...props
+}) => (
 	<Link href={href} key={key}>
 		<a
 			{...props}
+			{...(external
+				? { target: '_blank', rel: classNames(props.rel, 'noopener noreferrer') }
+				: undefined)}
 			className={classNames(
 				className,
 				'hover:text-blue-500 underline-animation transition-all relative after:bg-blue-500 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:hover:w-full after:transition-all'
