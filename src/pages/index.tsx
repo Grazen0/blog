358c7f057b27da10f0becc54be59c9b944822fd8
ElCholoGamer <josedanielgrayson@gmail.com>
2022-04-13 16:29:14 -0500

    refactor(homepage): increase heading line height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage<Props> = ({ latestPosts }) => {
 	return (
 		<Layout title="Home">
 			<main className="p-6">
-				<h1 className="text-5xl text-center font-bold my-8">👋 Welcome to my blog!</h1>
+				<h1 className="text-5xl text-center font-bold my-8 leading-snug">👋 Welcome to my blog!</h1>
 				<p className="text-center text-lg">Glad to see you around ;)</p>
 
 				<div className="max-w-2xl mx-auto my-12">
