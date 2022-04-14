d70059a00b70aab85d7a563e4068688bd72e0904
ElCholoGamer <josedanielgrayson@gmail.com>
2022-04-14 18:18:41 -0500

    feat(ci): post notifications

--- scripts/notify.js
+++ scripts/notify.js
@@ -4,8 +4,12 @@ const matter = require('gray-matter');
 const path = require('path');
 const YAML = require('yaml');
 
+if (!process.env.ADMIN_KEY) throw new Error('Missing admin key');
+
 const file = process.argv[2];
 if (!file) throw new Error('Filename not provided');
+
+console.log('Reading post file', file);
 
 const contents = readFileSync(file, 'utf8');
 const { data } = matter(contents);
@@ -13,7 +17,17 @@ const { data } = matter(contents);
 const dir = path.dirname(file);
 if (!dir.endsWith('posts')) {
 	const categoryFile = path.join(dir, 'category.yml');
+	console.log('Reading category file', categoryFile);
+
 	data.category = YAML.parse(readFileSync(categoryFile, 'utf8'));
 }
 
-console.log('Post data:', data);
+console.log('Sending POST request to server...');
+axios
+	.post('/api/notify', data, {
+		headers: {
+			Authorization: process.env.ADMIN_KEY,
+		},
+	})
+	.then(() => console.log(`Notified post "${data.title}"`))
+	.catch(console.error);
