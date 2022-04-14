c60b9cc77a6040d5bfb74e1b771d614d7a9e74ef
ElCholoGamer <josedanielgrayson@gmail.com>
2022-04-14 15:48:01 -0500

    fix(Spinner): change display to block

--- src/components/icons/Spinner.tsx
+++ src/components/icons/Spinner.tsx
@@ -5,7 +5,7 @@ export type Props = HTMLProps<HTMLSpanElement>;
 const Spinner: React.FC<Props> = props => (
 	<span
 		{...props}
-		className="inline-block w-7 h-7 border-4 border-white border-b-transparent rounded-full animate-spin"
+		className="block w-7 h-7 border-4 border-white border-b-transparent rounded-full animate-spin"
 	></span>
 );
 
