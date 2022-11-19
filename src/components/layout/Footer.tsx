8b623ba44c5ea2cfcaa6386260b3c1e4f604fa58
ElCholoGamer <josedanielgrayson@gmail.com>
2022-11-19 14:42:26 -0500

    refactor(components): add admin link to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,12 @@
+import Link from 'next/link';
+
 const Footer: React.FC = () => (
 	<footer className="p-4 text-center">
 		<small>Copyright &copy; {new Date().getFullYear()} ElCholoGamer. All rights reserved.</small>
+		<br />
+		<small className="text-slate-700">
+			<Link href="/admin">Admin</Link>
+		</small>
 	</footer>
 );
 
