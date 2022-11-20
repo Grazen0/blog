5385f50bc127a45241232fb1dd98aacd4c3a554e
ElCholoGamer <josedanielgrayson@gmail.com>
2022-11-19 22:45:58 -0500

    refactor: extract category state form to component

---
+++ src/components/CategoryForm.tsx
@@ -0,0 +1,128 @@
+import Image from 'next/image';
+import { ChangeEventHandler, FormEventHandler, HTMLProps, useState } from 'react';
+import classNames from 'classnames';
+import useDelayedState from 'lib/hooks/delayed-state';
+import { SetState } from 'lib/types';
+import Button from './Button';
+import TextInput from './form/TextInput';
+
+export interface CategoryFormState {
+	name: string;
+	description: string;
+	slug: string;
+	image: string;
+	imageAlt: string;
+}
+
+export interface Props extends Omit<HTMLProps<HTMLFormElement>, 'onSubmit'> {
+	submitDisabled: boolean;
+	setSubmitDisabled: SetState<boolean>;
+	onSubmit: (data: CategoryFormState) => void;
+	initialState?: CategoryFormState;
+	submitLabel: string;
+}
+
+const CategoryForm: React.FC<Props> = ({
+	className,
+	submitDisabled,
+	setSubmitDisabled,
+	onSubmit,
+	initialState,
+	submitLabel,
+	...props
+}) => {
+	const [formState, setFormState] = useState<CategoryFormState>({
+		name: initialState?.name || '',
+		description: initialState?.description || '',
+		slug: initialState?.slug || '',
+		image: initialState?.image || '',
+		imageAlt: initialState?.imageAlt || '',
+	});
+	const imagePreviewSrc = useDelayedState(formState.image);
+
+	const handleSubmit: FormEventHandler = e => {
+		e.preventDefault();
+		setSubmitDisabled(true);
+		onSubmit(formState);
+	};
+
+	const handleChange =
+		(
+			field: keyof typeof formState,
+			transform?: (value: string) => string
+		): ChangeEventHandler<HTMLInputElement> =>
+		e => {
+			setSubmitDisabled(false);
+			setFormState(state => ({
+				...state,
+				[field]: transform?.(e.target.value) || e.target.value,
+			}));
+		};
+
+	return (
+		<form
+			{...props}
+			onSubmit={handleSubmit}
+			className={classNames(className, 'max-w-2xl mx-auto my-16')}
+		>
+			<label htmlFor="name">Name</label>
+			<TextInput
+				id="name"
+				placeholder="Category name..."
+				value={formState.name}
+				required
+				onChange={handleChange('name', s => s.trimStart())}
+			/>
+			<label htmlFor="description">Description</label>
+			<TextInput
+				id="description"
+				placeholder="Category description..."
+				value={formState.description}
+				required
+				onChange={handleChange('description', s => s.trimStart())}
+			/>
+			<label htmlFor="slug">Slug</label>
+			<TextInput
+				id="slug"
+				placeholder="URL slug..."
+				value={formState.slug}
+				required
+				onChange={handleChange('slug', s => s.trimStart().toLowerCase().replaceAll(' ', '-'))}
+			/>
+
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4">
+				<div>
+					<label htmlFor="image-src">Image source</label>
+					<TextInput
+						id="image-src"
+						required
+						value={formState.image}
+						onChange={handleChange('image', s => s.trim())}
+						placeholder="Image URL..."
+					/>
+					<label htmlFor="image-alt">Alt text</label>
+					<TextInput
+						id="image-alt"
+						value={formState.imageAlt}
+						onChange={handleChange('imageAlt', s => s.trimStart())}
+						placeholder="Image alt text..."
+					/>
+				</div>
+				<div className="w-full h-48 xs:h-64 sm:h-full relative">
+					<Image
+						src={imagePreviewSrc}
+						alt={formState.imageAlt}
+						fill
+						className="object-cover rounded-md bg-neutral-300 dark:bg-slate-800 flex justify-center items-center"
+					/>
+				</div>
+			</div>
+
+			<Button type="submit" className="ml-auto my-12 block" disabled={submitDisabled}>
+				{submitLabel}
+			</Button>
+		</form>
+	);
+};
+
+export default CategoryForm;
--- src/pages/admin/categories/[category].tsx
+++ src/pages/admin/categories/[category].tsx
@@ -1,17 +1,17 @@
 import { GetServerSideProps, NextPage } from 'next';
 import Image from 'next/image';
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { useState } from 'react';
 import Button from 'components/Button';
-import TextInput from 'components/form/TextInput';
 import Layout from 'components/layout/Layout';
 import Category from 'lib/database/models/category';
 import { SerializedCategory } from 'lib/types';
 import BackLink from 'components/layout/BackLink';
 import Post from 'lib/database/models/post';
-import useDelayedState from 'lib/hooks/delayed-state';
 import { connect as db } from 'lib/database';
 import axios, { AxiosError } from 'axios';
 import Alert from 'components/Alert';
+import AnimatedLink from 'components/AnimatedLink';
+import CategoryForm, { CategoryFormState } from 'components/CategoryForm';
 
 interface Props {
 	categoryInfo: SerializedCategory;
@@ -41,34 +41,10 @@ const CategoryPanel: NextPage<Props> = ({ categoryInfo, postCount }) => {
 		src: categoryInfo.image,
 		alt: categoryInfo.imageAlt,
 	});
-	const [formState, setFormState] = useState({
-		name: categoryInfo.name,
-		description: categoryInfo.description,
-		slug: categoryInfo.slug,
-		image: categoryInfo.image,
-		imageAlt: categoryInfo.imageAlt,
-	});
-	const imagePreviewSrc = useDelayedState(formState.image);
 
-	const handleChange =
-		(
-			field: keyof typeof formState,
-			transform?: (value: string) => string
-		): ChangeEventHandler<HTMLInputElement> =>
-		e => {
-			setSubmitDisabled(false);
-			setFormState(state => ({
-				...state,
-				[field]: transform?.(e.target.value) || e.target.value,
-			}));
-		};
-
-	const handleSubmit: FormEventHandler = e => {
-		e.preventDefault();
-
-		setSubmitDisabled(true);
+	const handleSubmit = (data: CategoryFormState) => {
 		axios
-			.put(`/api/category/${categoryInfo.id}`, formState)
+			.put(`/api/category/${categoryInfo.id}`, data)
 			.then(({ data }) => {
 				setDisplayName(data.name);
 				setDisplayImage({
@@ -90,7 +66,10 @@ const CategoryPanel: NextPage<Props> = ({ categoryInfo, postCount }) => {
 				<BackLink href="/admin/categories">Categories</BackLink>
 				<h1 className="text-4xl font-bold mt-12 mb-4 text-center">{displayName}</h1>
 				<p className="text-center mb-8 text-xl">
-					{postCount} post{postCount !== 1 && 's'}
+					{postCount} post{postCount !== 1 && 's'} &middot;{' '}
+					<AnimatedLink href={`/posts/${categoryInfo.slug}`} className="font-semibold">
+						View
+					</AnimatedLink>
 				</p>
 				<figure className="text-center block my-12">
 					<Image
@@ -113,71 +92,20 @@ const CategoryPanel: NextPage<Props> = ({ categoryInfo, postCount }) => {
 				<hr />
 
 				<h2 className="text-2xl font-semibold text-center my-12">Edit category</h2>
-				<form onSubmit={handleSubmit} className="max-w-2xl mx-auto my-16">
-					<label htmlFor="name">Name</label>
-					<TextInput
-						id="name"
-						placeholder="Category name..."
-						value={formState.name}
-						required
-						onChange={handleChange('name', s => s.trimStart())}
-					/>
-					<label htmlFor="description">Description</label>
-					<TextInput
-						id="description"
-						placeholder="Category description..."
-						value={formState.description}
-						required
-						onChange={handleChange('description', s => s.trimStart())}
-					/>
-					<label htmlFor="slug">Slug</label>
-					<TextInput
-						id="slug"
-						placeholder="URL slug..."
-						value={formState.slug}
-						required
-						onChange={handleChange('slug', s => s.trimStart().toLowerCase().replaceAll(' ', '-'))}
-					/>
-
-					<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4">
-						<div>
-							<label htmlFor="image-src">Image source</label>
-							<TextInput
-								id="image-src"
-								required
-								value={formState.image}
-								onChange={handleChange('image', s => s.trim())}
-								placeholder="Image URL..."
-							/>
-							<label htmlFor="image-alt">Alt text</label>
-							<TextInput
-								id="image-alt"
-								value={formState.imageAlt}
-								onChange={handleChange('imageAlt', s => s.trimStart())}
-								placeholder="Image alt text..."
-							/>
-						</div>
-						<div className="w-full h-48 xs:h-64 sm:h-full relative">
-							<Image
-								src={imagePreviewSrc}
-								alt={formState.imageAlt}
-								fill
-								className="object-cover rounded-md bg-neutral-300 dark:bg-slate-800 flex justify-center items-center"
-							/>
-						</div>
+				<CategoryForm
+					onSubmit={handleSubmit}
+					submitDisabled={submitDisabled}
+					setSubmitDisabled={setSubmitDisabled}
+					initialState={categoryInfo}
+					submitLabel="Update"
+				/>
+				{status && (
+					<div className="text-center">
+						<Alert color={status.type === 'success' ? 'green' : 'red'} className="block mx-auto">
+							{status.message}
+						</Alert>
 					</div>
-
-					<Button type="submit" className="ml-auto my-12 block" disabled={submitDisabled}>
-						Update
-					</Button>
-					{status && (
-						<div className="text-center">
-							<Alert color={status.type === 'success' ? 'green' : 'red'} className="block mx-auto">
-								{status.message}
-							</Alert>
-						</div>
-					)}
-				</form>
+				)}
 			</main>
 		</Layout>
 	);
