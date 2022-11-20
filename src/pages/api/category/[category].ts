0450754ce21bbaba22831055556a786e6617b91a
ElCholoGamer <josedanielgrayson@gmail.com>
2022-11-19 22:45:20 -0500

    fix(api): add missing database connection call

--- src/pages/api/category/[category].ts
+++ src/pages/api/category/[category].ts
@@ -1,10 +1,12 @@
 import authenticate from 'lib/api/authenticate';
 import { createApiHandler } from 'lib/api/handler';
+import { connect as db } from 'lib/database';
 import Category from 'lib/database/models/category';
 
 const handler = createApiHandler();
 
 handler.put(authenticate(), async (req, res) => {
+	await db();
 	const category = await Category.findById(req.query.category?.toString());
 	if (!category) {
 		return res.status(404).json({
