b7b2a67defd4f3f0e88abc9cac66c7e2f4eb7a78
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-09 23:59:38 -0500

    refactor: use giscus react component

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,39 +1,27 @@
+import Giscus from '@giscus/react';
 import { useDarkMode } from 'lib/providers/dark-mode';
-import { HTMLProps, useEffect, useRef } from 'react';
+import { HTMLProps } from 'react';
 
 const Comments: React.FC<HTMLProps<HTMLDivElement>> = props => {
 	const [darkMode] = useDarkMode();
-	const containerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		const container = containerRef.current;
-		if (!container) return;
-
-		const script = document.createElement('script');
-		script.src = 'https://giscus.app/client.js';
-		script.async = true;
-		script.crossOrigin = 'anonymous';
-		script.setAttribute('data-repo', 'ElCholoGamer/blog');
-		script.setAttribute('data-repo-id', 'R_kgDOG5iLiQ');
-		script.setAttribute('data-category', 'Comments');
-		script.setAttribute('data-category-id', 'DIC_kwDOG5iLic4CS8tV');
-		script.setAttribute('data-mapping', 'pathname');
-		script.setAttribute('data-strict', '1');
-		script.setAttribute('data-reactions-enabled', '1');
-		script.setAttribute('data-emit-metadata', '0');
-		script.setAttribute('data-input-position', 'top');
-		script.setAttribute('data-theme', darkMode ? 'dark_dimmed' : 'light');
-		script.setAttribute('data-lang', 'en');
-
-		container.appendChild(script);
-
-		return () => {
-			script.remove();
-			container.textContent = '';
-		};
-	}, [darkMode]);
-
-	return <div {...props} ref={containerRef}></div>;
+	return (
+		<div {...props}>
+			<Giscus
+				repo="ElCholoGamer/blog"
+				repoId="R_kgDOG5iLiQ"
+				category="Comments"
+				categoryId="DIC_kwDOG5iLic4CS8tV"
+				mapping="pathname"
+				strict="1"
+				reactionsEnabled="1"
+				emitMetadata="0"
+				inputPosition="top"
+				theme={darkMode ? 'dark_dimmed' : 'light'}
+				lang="en"
+			/>
+		</div>
+	);
 };
 
 export default Comments;
